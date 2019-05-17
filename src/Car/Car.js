73c9a138385375462ade07dd5f5525d087292789
SuperBigBang <superbigbang@yandex.ru>
2019-05-18 00:14:49 +0900

    feat: complete 10s

---
+++ src/Car/Car.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+/*function car() {
+    return(
+        <div>This is car component</div>
+    );
+}*/
+
+/*const car = () => {return(<div>This is car component</div>);};*/
+
+/*const car = () => (
+    <div>This is car component
+<strong>test strong</strong>
+    </div>);*/
+
+export default () => (
+    <div>
+        <p>This is car component</p>
+        <p>Number: <strong>{Math.round(Math.random()*100)}</strong></p>
+    </div>);
