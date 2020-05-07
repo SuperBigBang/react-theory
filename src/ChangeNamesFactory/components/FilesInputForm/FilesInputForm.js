751407c4eefe90f113fd6140bdcb5a2147ec592d
vitalykozeev <vitalykozeev@gmail.com>
2020-05-07 18:38:02 +0900

    feat: сделан необходимый вывод строк для Mjsty

--- src/ChangeNamesFactory/components/FilesInputForm/FilesInputForm.js
+++ src/ChangeNamesFactory/components/FilesInputForm/FilesInputForm.js
@@ -8,30 +8,35 @@ export default function FilesInputForm(props) {
     let makeURLForSaveNewFileHandler = props.makeURLForSaveNewFileHandler;
     const onDrop = useCallback(acceptedFiles => {
         // Do something with the files
+        let string_massive = "";
         for (let i = 0; i < acceptedFiles.length; i++) {
-            console.log(acceptedFiles[i].name);
+            string_massive+=((((acceptedFiles[i].name).replace(".WAV", "")).replace(".wav", ""))+"\tE:\\HRGameMod\\Data\\complete\\"+acceptedFiles[i].name)+'\n';
+            //console.log(acceptedFiles)
+            //console.log(acceptedFiles[i].name);
         }
-        console.log("Всего принято файлов: " + acceptedFiles.length);
+        console.log(string_massive)
+
+        //console.log("Всего принято файлов: " + acceptedFiles.length);
 
 
-        let zip = new JSZip();
-
-        for (let i = 0; i < acceptedFiles.length; i++) {
-            // console.log("Проходы по файлам: " + i);
-            for (let j = 0; j < bigData.length; j++) {
-                // console.log("Проходы по базе: " + j);
-                const bigDataElement = bigData[j];
-                if (bigDataElement.split(";")[0].toUpperCase() === acceptedFiles[i].name.toUpperCase().split(".JPG")[0]) {
-                    addToZIP(zip, acceptedFiles[i], bigDataElement.split(";")[1] + ".JPG");
-                    break;
-                }
-            }
-        }
-
-        zip.generateAsync({type: "blob"})
-            .then(function (blob) {
-                makeURLForSaveNewFileHandler(blob)
-            });
+        // let zip = new JSZip();
+        //
+        // for (let i = 0; i < acceptedFiles.length; i++) {
+        //     // console.log("Проходы по файлам: " + i);
+        //     for (let j = 0; j < bigData.length; j++) {
+        //         // console.log("Проходы по базе: " + j);
+        //         const bigDataElement = bigData[j];
+        //         if (bigDataElement.split(";")[0].toUpperCase() === acceptedFiles[i].name.toUpperCase().split(".JPG")[0]) {
+        //             addToZIP(zip, acceptedFiles[i], bigDataElement.split(";")[1] + ".JPG");
+        //             break;
+        //         }
+        //     }
+        // }
+        //
+        // zip.generateAsync({type: "blob"})
+        //     .then(function (blob) {
+        //         makeURLForSaveNewFileHandler(blob)
+        //     });
 
 
     }, []);
